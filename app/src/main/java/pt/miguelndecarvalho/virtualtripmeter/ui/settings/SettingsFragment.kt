c002293bdb9a02af13526d1f1eb35b2d8186f2cc
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-04-09 16:38:18 +0100

    Refactor Notifications tab to Settings

--- app/src/main/java/pt/miguelndecarvalho/virtualtripmeter/ui/settings/SettingsFragment.kt
+++ app/src/main/java/pt/miguelndecarvalho/virtualtripmeter/ui/settings/SettingsFragment.kt
@@ -1,4 +1,4 @@
-package pt.miguelndecarvalho.virtualtripmeter.ui.notifications
+package pt.miguelndecarvalho.virtualtripmeter.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import pt.miguelndecarvalho.virtualtripmeter.R
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        settingsViewModel =
+                ViewModelProvider(this).get(SettingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
