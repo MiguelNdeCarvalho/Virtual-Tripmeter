0ad7c80e8411510bda50e0b56d039a6251e8e55e
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-04-11 12:41:45 +0100

    SettingsFragment.kt: Drop related to TexView

--- app/src/main/java/pt/miguelndecarvalho/virtualtripmeter/ui/settings/SettingsFragment.kt
+++ app/src/main/java/pt/miguelndecarvalho/virtualtripmeter/ui/settings/SettingsFragment.kt
@@ -21,11 +21,6 @@ class SettingsFragment : Fragment() {
     ): View? {
         settingsViewModel =
                 ViewModelProvider(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
