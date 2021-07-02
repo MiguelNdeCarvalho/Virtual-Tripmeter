82916e3a844dbb1918cc9cf6f1d97951cff2502e
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-07-02 23:26:09 +0100

    TripFragment.kt: Updates on the Backend (2/2)

--- app/src/main/java/pt/miguelndecarvalho/virtualtripmeter/ui/trip/TripFragment.kt
+++ app/src/main/java/pt/miguelndecarvalho/virtualtripmeter/ui/trip/TripFragment.kt
@@ -23,25 +23,32 @@ import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import pt.miguelndecarvalho.virtualtripmeter.R
 
 class TripFragment : Fragment() {
 
     private lateinit var tripViewModel: TripViewModel
-    var total= 000.00
-    var partial= 000.00
-    private var toggleImperialUnits:Boolean = false
-    private var baseSpeed:String = ""
-    private var toggleAlertSounds:Boolean = false
+    var total = 000.00
+    var partial = 000.00
+    private var toggleImperialUnits: Boolean = false
+    private var baseSpeed: String = ""
+    private var toggleAlertSounds: Boolean = false
+    private var running = false
+    private var pauseFlag = false
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         tripViewModel =
-                ViewModelProvider(this).get(TripViewModel::class.java)
+            ViewModelProvider(this).get(TripViewModel::class.java)
 
         var mView = inflater.inflate(R.layout.fragment_trip, container, false)
 
@@ -50,8 +57,7 @@ class TripFragment : Fragment() {
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    fun getSettings(mView: View)
-    {
+    fun getSettings(mView: View) {
         val user = Firebase.auth.currentUser
         val userID = user!!.uid
         val db = Firebase.firestore
@@ -65,11 +71,13 @@ class TripFragment : Fragment() {
                     toggleAlertSounds = document.getBoolean("toggleAlertSounds") == true
                     mView.findViewById<TextView>(R.id.currentVelocity).text = baseSpeed
 
-                    if(toggleImperialUnits)
-                    {
-                        mView.findViewById<TextView>(R.id.totalKM).text = getString(R.string.trip_unitMiles)
-                        mView.findViewById<TextView>(R.id.partialKM).text = getString(R.string.trip_unitMiles)
-                        mView.findViewById<TextView>(R.id.velocityUnity).text = getString(R.string.trip_unitMilesHour)
+                    if (toggleImperialUnits) {
+                        mView.findViewById<TextView>(R.id.totalKM).text =
+                            getString(R.string.trip_unitMiles)
+                        mView.findViewById<TextView>(R.id.partialKM).text =
+                            getString(R.string.trip_unitMiles)
+                        mView.findViewById<TextView>(R.id.velocityUnity).text =
+                            getString(R.string.trip_unitMilesHour)
                     }
                     setupButtons(mView)
                 }
@@ -77,14 +85,13 @@ class TripFragment : Fragment() {
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    fun setupButtons(mView: View)
-    {
+    fun setupButtons(mView: View) {
         val totalSound = MediaPlayer.create(context, R.raw.totalalert);
         val partialSound = MediaPlayer.create(context, R.raw.partialalert)
 
         // Trips
 
-        mView.findViewById<Button>(R.id.totalButton).setOnLongClickListener{
+        mView.findViewById<Button>(R.id.totalButton).setOnLongClickListener {
             val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
             val effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
 
@@ -99,7 +106,7 @@ class TripFragment : Fragment() {
             return@setOnLongClickListener true
         }
 
-        mView.findViewById<Button>(R.id.partialButton).setOnClickListener{
+        mView.findViewById<Button>(R.id.partialButton).setOnClickListener {
             val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
             val effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK)
 
@@ -112,13 +119,13 @@ class TripFragment : Fragment() {
         }
 
         // Velocity Controls
-        mView.findViewById<ImageButton>(R.id.decrementButton).setOnClickListener{
+        mView.findViewById<ImageButton>(R.id.decrementButton).setOnClickListener {
             var temp = baseSpeed.toInt() - 1
             baseSpeed = temp.toString()
             mView.findViewById<TextView>(R.id.currentVelocity).text = baseSpeed
         }
 
-        mView.findViewById<ImageButton>(R.id.decrementButton).setOnLongClickListener{
+        mView.findViewById<ImageButton>(R.id.decrementButton).setOnLongClickListener {
             var temp = baseSpeed.toInt() - 10
             baseSpeed = temp.toString()
             mView.findViewById<TextView>(R.id.currentVelocity).text = baseSpeed
@@ -126,13 +133,13 @@ class TripFragment : Fragment() {
             return@setOnLongClickListener true
         }
 
-        mView.findViewById<ImageButton>(R.id.incrementButton).setOnClickListener{
+        mView.findViewById<ImageButton>(R.id.incrementButton).setOnClickListener {
             var temp = baseSpeed.toInt() + 1
             baseSpeed = temp.toString()
             mView.findViewById<TextView>(R.id.currentVelocity).text = baseSpeed
         }
 
-        mView.findViewById<ImageButton>(R.id.incrementButton).setOnLongClickListener{
+        mView.findViewById<ImageButton>(R.id.incrementButton).setOnLongClickListener {
             var temp = baseSpeed.toInt() + 10
             baseSpeed = temp.toString()
             mView.findViewById<TextView>(R.id.currentVelocity).text = baseSpeed
@@ -140,14 +147,53 @@ class TripFragment : Fragment() {
             return@setOnLongClickListener true
         }
 
-        /* Control Buttons (Play,Pause)
-        mView.findViewById<ImageButton>(R.id.start_button).setOnClickListener{
-            // TODO: 7/2/21  
+        //Controls
+        mView.findViewById<ImageButton>(R.id.playpauseButton).setOnClickListener {
+
+            if (!running)
+            {
+                mView.findViewById<ImageButton>(R.id.playpauseButton).setImageResource(R.drawable.pause_black_24dp)
+                CoroutineScope(IO).launch {
+                    startTrip(mView)
+                }
+            }
+            else if (running)
+            {
+                pauseFlag = true
+                mView.findViewById<ImageButton>(R.id.playpauseButton).setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            }
         }
 
-        mView.findViewById<ImageButton>(R.id.stopButton).setOnClickListener{
-            // TODO: 7/2/21
-        }*/
+        mView.findViewById<ImageButton>(R.id.stopButton).setOnClickListener {
+            running = false
+            total = 0.0
+            partial = 0.0
 
+            mView.findViewById<TextView>(R.id.totalButton).text = "%.2f".format(total)
+            mView.findViewById<TextView>(R.id.partialButton).text = "%.2f".format(partial)
+            mView.findViewById<ImageButton>(R.id.playpauseButton).setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        }
+
+    }
+
+    private suspend fun startTrip(mView: View){
+        running = true
+
+        while (running)
+        {
+            if(pauseFlag)
+            {
+                running = false
+                pauseFlag = false
+                break
+            }
+
+            var speedSecond = baseSpeed.toFloat() / 3600
+            total += speedSecond
+            partial += speedSecond
+            mView.findViewById<TextView>(R.id.totalButton).text = "%.2f".format(total)
+            mView.findViewById<TextView>(R.id.partialButton).text = "%.2f".format(partial)
+            delay(1000)
+        }
     }
 }
